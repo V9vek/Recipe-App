6dfc931e033c94249cbe54a87c21c913bf92562f
Vivek Sharma <viveksharma2382000@gmail.com>
2021-12-22 00:02:40 +0530

    Add Usecase for screens

---
+++ app/src/main/java/com/vivek/recipeapp/domain/data/DataState.kt
@@ -0,0 +1,52 @@
+package com.vivek.recipeapp.domain.data
+
+/**
+ * Data
+ * Loading
+ * Error
+ */
+
+data class DataState<out T>(
+    val data: T? = null,
+    val error: String? = null,
+    val loading: Boolean = false,
+) {
+
+    companion object {
+        fun <T> success(data: T): DataState<T> {
+            return DataState(data = data)
+        }
+
+        fun <T> error(message: String): DataState<T> {
+            return DataState(error = message)
+        }
+
+        fun <T> loading(): DataState<T> {
+            return DataState(loading = true)
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
